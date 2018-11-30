2b6af913a69183960928a9752e9ebaef49be52d4
Nino Yosinao <nino.yosinao@gmail.com>
2018-11-29 19:57:53 -0800

    modified files in preparation for Leaflet rendering

--- app/static/js/config.js
+++ app/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "sk.eyJ1Ijoibmlub3lvc2luYW8iLCJhIjoiY2pvYnJtc2hnMjV3cjNrczF1cGVoejR5bSJ9.Z-iMYIzKWvFZ2LnaAWFpEA";
+const API_KEY = "pk.eyJ1Ijoibmlub3lvc2luYW8iLCJhIjoiY2pvYnJrdTlnMGUwNjNxbXM3MWk2b3F0aCJ9.wRU07tSzenzJoImy1Pz5tQ";
