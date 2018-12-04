32779927711cbdead1b0ee1de9a228d32a161e36
Nino Yosinao <nino.yosinao@gmail.com>
2018-12-03 22:26:43 -0800

    updated logic.js

--- app/static/js/config.js
+++ app/static/js/config.js
@@ -1,6 +1,2 @@
 // API key
-<<<<<<< HEAD
-const API_KEY = "pk.eyJ1Ijoibmlub3lvc2luYW8iLCJhIjoiY2pvYnJrdTlnMGUwNjNxbXM3MWk2b3F0aCJ9.wRU07tSzenzJoImy1Pz5tQ";
-=======
 const API_KEY = "pk.eyJ1IjoiZmluaWFuZG9uZWlsbCIsImEiOiJjanAzZmRwMnQwaGZ2M2tvYm5nNGNqcmE4In0.XAVtWcAaSFjXYCZRxkA6iw";
->>>>>>> 24930bfcaa8f84129a08b4dfc6fa1b552843c502
